277a9c033854cb86c77da33ff5194e6de2d48d41
Miguelro123 <969@holbertonschool.com>
2019-09-27 02:34:14 +0000

    0. push, pall

--- monty.h
+++ monty.h
@@ -1,42 +1,67 @@
-#ifndef _MONTY_H_
-#define _MONTY_H_
+#ifndef MONTY
+#define MONTY
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <ctype.h>
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * struct stack_s - Doubly linked list representation of a stack (or queue).
+ * @n: Integer.
+ * @prev: Points to the previous element of the stack (or queue).
+ * @next: Points to the next element of the stack (or queue).
  *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * Description: Doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project.
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
+ * struct instruction_s - Opcoode and its function.
+ * @opcode: The opcode.
+ * @f: Function to handle the opcode.
  *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * Description: Opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project.
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void _push(stack_t **stack, unsigned int line_number);
-void _pall(stack_t **stack, unsigned int line_number);
+/**
+ * struct var_struct - Struct for global variable.
+ * @arg: Arguments.
+ * @input_str: Input string.
+ * @SQ: Check for stack or queue.
+ * @file: File descripter.
+ */
+typedef struct var_struct
+{
+	char *arg;
+	char *input_str;
+	int SQ;
+	FILE *file;
+} global_v;
+
+extern global_v arg_holder;
+global_v arg_holder;
+
+void make_buffer(char *file_name);
+void opcode(char *command, unsigned int line_num, stack_t **stack);
+void free_stack(stack_t **head);
+void push(stack_t **stack, unsigned int line_num);
+void pall(stack_t **stack, unsigned int line_num);
 
 #endif
